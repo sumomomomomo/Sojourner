20421955c2d8a301bb269d473403bf88cac3431c
Matthew Ho Phang Hao <matthew.hoph@outlook.sg>
2024-06-07 16:22:51 +0800

    Change battle boundaries from canvas to gameobjects

--- orbital-24-game/Assets/Code/Scripts/Battle/PlayerBattle.cs
+++ orbital-24-game/Assets/Code/Scripts/Battle/PlayerBattle.cs
@@ -1,61 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerBattle : MonoBehaviour
 {
-    // For correctly determining bounds
-    private Camera mainCamera;
-    private Vector3 StartingPos = Vector3.zero;
     [SerializeField] private float speed;
-    [SerializeField] private float sensitivity;
-    private Vector2 MovePos;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Collider2D playerWallCollider;
 
-    [SerializeField] private FloatReference borderThickness;
+    // private Vector2 prevPosition;
 
-    [SerializeField] private FloatReference boundWidth;
-    [SerializeField] private FloatReference boundHeight;
-    [SerializeField] private FloatReference boundOriginXTranslation;
-    [SerializeField] private FloatReference boundOriginYTranslation;
+    //private ContactFilter2D wallFilter;
+    // private Collider2D[] temp;
 
-    //private Vector3 transformScreenPosition;
+    //private bool isOverlapWall;
 
-    private Vector3 worldBoundsTopRight;
-    private Vector3 worldBoundsBottomLeft;
-    private Vector3 worldBoundsZero;
 
     void Start()
     {
-        transform.position = StartingPos;
-        MovePos = StartingPos;
-        mainCamera = Camera.main;
-        worldBoundsZero = mainCamera.WorldToScreenPoint(new Vector3(boundOriginXTranslation.Value, boundOriginYTranslation.Value, 0));
-        Debug.Log(worldBoundsZero);
+        rb.gravityScale = 0;
+        rb.freezeRotation = true;
+        // prevPosition = rb.position;
+        // temp = new Collider2D[1];
+        // wallFilter.minDepth = -1;
+        // wallFilter.maxDepth = -1;
+        // wallFilter.useDepth = true;
     }
 
-    // Like update but runs 50 times per second only
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal") * sensitivity;
-        float vertical = Input.GetAxis("Vertical") * sensitivity;
+        // isOverlapWall = Physics2D.OverlapCollider(playerWallCollider, wallFilter, temp) > 0;
+        // if (isOverlapWall)
+        // {
+        //     rb.position = prevPosition;
+        //     rb.velocity = Vector2.zero;
+        // }
+        // else
+        // {
+        // prevPosition.x = rb.position.x;
+        // prevPosition.y = rb.position.y;
 
-        MovePos.x += horizontal;
-        MovePos.y += vertical;
+        float horizontal = Input.GetAxisRaw("Horizontal") * speed;
+        float vertical = Input.GetAxisRaw("Vertical") * speed;
+
+        //rb.MovePosition(new Vector2(rb.position.x + horizontal, rb.position.y + vertical));
+        rb.velocity = new Vector2(horizontal, vertical);
+        // }
 
         //transformScreenPosition = mainCamera.WorldToScreenPoint(transform.position) - mainCamera.WorldToScreenPoint(new Vector3(0, 0, 0));
 
-        worldBoundsTopRight = mainCamera.ScreenToWorldPoint(new Vector3(
-            worldBoundsZero.x + boundWidth.Value / 2, 
-            worldBoundsZero.y + boundHeight.Value / 2, 
-            -10));
-        worldBoundsBottomLeft = mainCamera.ScreenToWorldPoint(new Vector3(
-            worldBoundsZero.x - boundWidth.Value / 2, 
-            worldBoundsZero.y - boundHeight.Value / 2, 
-            -10));
+        // worldBoundsTopRight = mainCamera.ScreenToWorldPoint(new Vector3(
+        //     worldBoundsZero.x + boundWidth.Value / 2, 
+        //     worldBoundsZero.y + boundHeight.Value / 2, 
+        //     -10));
+        // worldBoundsBottomLeft = mainCamera.ScreenToWorldPoint(new Vector3(
+        //     worldBoundsZero.x - boundWidth.Value / 2, 
+        //     worldBoundsZero.y - boundHeight.Value / 2, 
+        //     -10));
 
-        MovePos.x = Mathf.Clamp(MovePos.x, worldBoundsBottomLeft.x, worldBoundsTopRight.x);
-        MovePos.y = Mathf.Clamp(MovePos.y, worldBoundsBottomLeft.y, worldBoundsTopRight.y);
+        // MovePos.x = Mathf.Clamp(MovePos.x, worldBoundsBottomLeft.x, worldBoundsTopRight.x);
+        // MovePos.y = Mathf.Clamp(MovePos.y, worldBoundsBottomLeft.y, worldBoundsTopRight.y);
 
-        transform.position = Vector2.Lerp(transform.position, MovePos, speed * Time.deltaTime);
+        //transform.position = Vector2.Lerp(transform.position, MovePos, speed * Time.deltaTime);
     }
+
+    // void LateUpdate()
+    // {
+    //     Debug.Log(prevPosition);
+    //     bool isOverlapWall = Physics2D.OverlapCollider(playerWallCollider, wallFilter, temp) > 0;
+    //     if (isOverlapWall)
+    //     {
+    //         rb.position = prevPosition;
+    //         rb.velocity = Vector2.zero;
+    //     }
+    //     else
+    //     {
+    //         prevPosition.x = rb.position.x;
+    //         prevPosition.y = rb.position.y;
+    //     }
+    // }
 }
