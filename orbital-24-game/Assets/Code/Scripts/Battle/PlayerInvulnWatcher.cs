eed48fb9ce44e8775d89ca58fcfbede2d4e7dd03
Matthew Ho Phang Hao <matthew.hoph@outlook.sg>
2024-06-18 17:54:46 +0800

    Invuln animation

--- orbital-24-game/Assets/Code/Scripts/Battle/PlayerInvulnWatcher.cs
+++ orbital-24-game/Assets/Code/Scripts/Battle/PlayerInvulnWatcher.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 
 public class PlayerInvulnWatcher : MonoBehaviour
 {
+    private Animator playerAnimator;
     [SerializeField] private TMP_Text invulnTimer;
     [SerializeField] private FloatReference timeLeftForInvuln;
     [SerializeField] private BoolReference isInvuln;
+
+    void Start()
+    {
+        playerAnimator = GetComponentInParent<Animator>();
+    }
     void Update()
     {
         if (isInvuln.Value)
         {
             invulnTimer.text = "Invuln for " + ((int) Math.Floor(timeLeftForInvuln.Value)).ToString() + "s";
+            if (playerAnimator != null) playerAnimator.Play("Invuln Blink");
         }
         else
         {
             invulnTimer.text = "";
+            if (playerAnimator != null) playerAnimator.Play("Normal");
         }
     }
 }
